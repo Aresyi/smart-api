b36c6d5008aa1651db33c03e1a8306ecc6259cce
宋同煜 <songtongyu@9tongmail.com>
2017-08-04 11:10:11 +0800

    【优化】将servlet-api 依赖打开，避免因人而异的环境问题导致找不到servlet 类

--- src/main/java/com/ydj/smart/startup/Bootstrap.java
+++ src/main/java/com/ydj/smart/startup/Bootstrap.java
@@ -1,19 +1,19 @@
 package com.ydj.smart.startup;
 
-import java.io.PrintStream;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.PrintStream;
+
 /**
 *
 * @author : Ares.yi
-* @createTime : 2014-11-10 上午11:13:42 
+* @createTime : 2017-8-1 上午11:13:42
 * @version : 1.0 
-* @description : FIXME : 暂未完善
+* @description : 这个类的作用与在web.xml中配置负责初始化Spring应用上下文的监听器作用类似，只不过在这里不需要编写额外的XML文件了。
 *
  */
 @EnableAutoConfiguration
